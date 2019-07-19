e070add464cf25b31a885461c2c2ea95cb44a93e
Brian Carroll <bcarroll@squarespace.com>
2019-07-19 12:46:49 -0400

    Initialize for video background example

--- scripts/site.js
+++ scripts/site.js
@@ -1,20 +1,12 @@
-
-// Use the sqs-core module to access core Squarespace
-// functionality, like Lifecycle and ImageLoader. For
-// full documentation, go to:
-//
-// http://github.com/squarespace/squarespace-core
-
-const core = require('@squarespace/core');
+// Import the module here
 
 window.addEventListener('DOMContentLoaded', function() {
 
-  const images = document.querySelectorAll('img[data-src]');
-
-  for (let i = 0; i < images.length; i++) {
-    core.ImageLoader.load(images[i], {
-      load: true
-    });
+  function initializeVideoBackground() {
+    // Your code goes here
   }
+
+
+  initializeVideoBackground();
 
 });
